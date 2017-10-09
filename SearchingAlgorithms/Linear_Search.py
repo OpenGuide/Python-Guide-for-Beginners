b35e36afcd7fc4c1496b38e51413a1abf2d79096
Vinícius Piloto <vinicius@hangar360.com.br>
2017-10-09 13:51:07 -0300

    Update Linear_Search.py
    Add comments to Linear_Search.py

--- SearchingAlgorithms/Linear_Search.py
+++ SearchingAlgorithms/Linear_Search.py
@@ -1,9 +1,15 @@
+# Linear search function that returns 'True' if the item is in the list 'data' of 'False' otherwise
+
 def linearSearch(item, data):
+  # Start with position 0 and not found the element yet
   position = 0
-  found = 0
+  found = False
   
+  # Check if item is in the list from position 0 to the end of the list
+  # When element is found, change the value of the variable 'found' to 'True'
   while position < len(data) and not found:
     if(data[position] == item):
       found = True
     position += 1
+    
   return found
