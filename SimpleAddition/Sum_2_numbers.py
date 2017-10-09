2273d0dc7995494050af0b2cb40e3f337e590deb
Priefy <peachy-lovly-mom@hotmail.com>
2017-10-09 22:56:15 +0700

    add sum 2 numbers

--- SimpleAddition/Sum_2_numbers.py
+++ SimpleAddition/Sum_2_numbers.py
@@ -1,1 +1,16 @@
+while True:
+    try:
+        print("Enter the value of X and press Enter")
+        x = float(input('X: '))
+
+        print("Enter the value of Y and press Enter")
+        y = float(input('Y: '))
+
+        # X+Y
+        print('X+Y: ', x + y)
+
+        print("========== End ==========")
+
+    except ValueError:
+        print('Please input an INTEGER...')
 
