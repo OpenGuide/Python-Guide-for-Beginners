e8933f3b5c7a6b64d1bba3f0cacefce6830a8ea1
Manvi Goel <manvi16244@iiitd.ac.in>
2017-10-09 22:39:20 +0530

    linear search using map function

--- SearchingAlgorithms/Linear_Search.py
+++ SearchingAlgorithms/Linear_Search.py
@@ -1,1 +1,17 @@
+# linear search of an item in the list inputted by the user (python 2.7)
+print("Enter numbers to be stored in a list (space separated")
 
+""" map function in order to store the values of the two space separated integers
+	map (function, iterable..) applies the function on all the items of the iterable """
+
+l = map(int, raw_input().split())   #enter space separated entries in a list
+item = input()                      #item to be searched for
+found = 0                           #check to find if th eitem has been found or not
+
+for i in range(len(l)):             #iterating through the array
+	if(l[i] == item):                 #if element found
+		found = 1
+		print('Item found in the list at index position : ' + str(i)) 
+		break
+if(found == 0):                     #if element not found
+	print("Sorry, Item not found in the list ")
