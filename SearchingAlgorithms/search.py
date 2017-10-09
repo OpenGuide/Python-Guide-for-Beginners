002265f186d3ba53b6007abf7b9b5a2d7b7f2555
emdavis7 <ericd@simpli.fi>
2017-10-09 14:41:09 -0500

    Name fix and added comments

--- SearchingAlgorithms/search.py
+++
@@ -1,6 +0,0 @@
-def search(search_term, search_list):
-    """Finds if search term (string) exist in list"""
-    for term in search_list:
-        if search_term in term:
-            return True
-        return False
