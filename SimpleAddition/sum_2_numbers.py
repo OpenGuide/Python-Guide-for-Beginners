9d109a8ebf35207a65df1886723984136da6efdf
Bhk <bharatraghunthan9767@gmail.com>
2017-10-09 22:34:20 +0530

    Simple sum of two numbers with comments

--- SimpleAddition/sum_2_numbers.py
+++ SimpleAddition/sum_2_numbers.py
@@ -1,2 +1,10 @@
+'''
+Inputs a string, uses the split() function to remove whitespaces
+and then uses map() function to map them to integers.
+'''
 a,b = map(int, input("Enter two numbers: ").split())
+'''
+Outputs the sum after using a format specifier %d 
+to denote a placeholder for an integer.
+'''
 print("The sum is %d\n"%(a+b))
