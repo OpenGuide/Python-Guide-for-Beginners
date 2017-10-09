c086f96c2e0c600d2f71bd8b0b05019c1b41fec6
Manvi Goel <manvi16244@iiitd.ac.in>
2017-10-09 20:42:00 +0530

    Code for sum of two integers

--- SimpleAddition/Sum_2_numbers.py
+++ SimpleAddition/Sum_2_numbers.py
@@ -1,1 +1,3 @@
-
+# python code to add two numbers
+a, b = map(int, raw_input().split()) # enter two space separated integers
+print(a + b)                         # displays the sum of the two integers
