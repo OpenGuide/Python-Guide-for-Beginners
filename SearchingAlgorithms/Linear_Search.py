d10d5fb31dfa125d669542fdfbdd38bb6931148d
Priefy <peachy-lovly-mom@hotmail.com>
2017-10-09 22:43:35 +0700

    add linear search function

--- SearchingAlgorithms/Linear_Search.py
+++ SearchingAlgorithms/Linear_Search.py
@@ -1,1 +1,19 @@
+def linearSearch(item, list):
+    found = False
+    position = 0
 
+    while position < len(list) and not found:
+        if list[position] == item:
+            found = True
+        position = position + 1
+    return found
+
+
+if __name__ == "__main__":
+    itemList = ["apple", "banana", "cocoa", "durian"]
+    input = input("what item do you want to find?")
+    isItFound = linearSearch(input, itemList)
+    if isItFound:
+        print("your item is in the list!")
+    else:
+        print("your item is not in the list!")
