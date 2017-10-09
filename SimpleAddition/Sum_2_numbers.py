cc0f67748f3764a939faeb76ce9e3dee0de1eafa
Manvi Goel <manvi16244@iiitd.ac.in>
2017-10-09 22:16:53 +0530

    sum of two numbers using map function

--- SimpleAddition/Sum_2_numbers.py
+++ SimpleAddition/Sum_2_numbers.py
@@ -1,3 +1,9 @@
 # python code to add two numbers
+print("Add two numbers that you want to add. (space separated)")
+
+""" map function in order to store the values of the two space separated integers
+	map (function, iterable..) applies the function on all the items of the iterable """
+
 a, b = map(int, raw_input().split()) # enter two space separated integers
-print(a + b)                         # displays the sum of the two integers
+
+print("The sum of the two numbers is : " + (str) (a +b))                         # displays the sum of the two integers
