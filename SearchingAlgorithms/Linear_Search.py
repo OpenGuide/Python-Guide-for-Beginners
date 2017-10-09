8f591fdb2b01c7b9041ff9a837e25552c0a47c2b
Vinícius Piloto <vinicius@hangar360.com.br>
2017-10-09 12:04:14 -0300

    Update Linear_Search.py

--- SearchingAlgorithms/Linear_Search.py
+++ SearchingAlgorithms/Linear_Search.py
@@ -1,1 +1,9 @@
-
+def linearSearch(item, data):
+  position = 0
+  found = 0
+  
+  while position < len(data) and not found:
+    if(data[position] == item):
+      found = True
+    position += 1
+  return found
