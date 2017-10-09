b89a7d856e744d89b475f02f231a3d97c9d8e20b
Vinícius Piloto <vinicius@hangar360.com.br>
2017-10-09 11:54:25 -0300

    Update Sum_2_numbers.py

--- SimpleAddition/Sum_2_numbers.py
+++ SimpleAddition/Sum_2_numbers.py
@@ -1,1 +1,8 @@
-
+def maxNumber(a,b):
+  if(a > b):
+    print(str(a)+" is greater than "+str(b))
+  elif(b > a):
+    print(str(b)+" is greater than "+str(a))
+  elif(a == b):
+    print(str(a)+" is equal to "+str(b))
+    
