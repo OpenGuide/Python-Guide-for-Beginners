57265d117da9d94756bf8964c6b36ea6d8b4dafa
ThePayman <thepayman@sapo.pt>
2017-10-09 16:24:24 +0100

    Update Sum_2_numbers.py

--- SimpleAddition/Sum_2_numbers.py
+++ SimpleAddition/Sum_2_numbers.py
@@ -1,1 +1,8 @@
-
+def add_numbers(*args):
+    sum = 0
+    for arg in args:
+        try:
+            sum += int(arg)
+        except:
+            print(">"+arg+"<" + " isnt a valid number")
+    print(sum)
