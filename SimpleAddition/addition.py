56362688b9dc58d4dc1fc143c64a147f3252ed50
E_M <eshita.mann100@gmail.com>
2017-10-12 19:50:50 +0530

    Add files via upload

---
+++ SimpleAddition/addition.py
@@ -0,0 +1,8 @@
+#Addition of 2 numbers
+c='y'
+while c=='y' or c=='Y':
+    a=float(input("Enter 1st no:"))
+    b=float(input("Enter 2nd no:"))
+    print(a+b)
+    c=input("Continue(y/n):")
+print("BYE :)")
