d50fd956dbcdb012c757268e145a88a795e12560
Jennifer Goodridge <jen@jengoodridge.com>
2017-10-22 01:29:22 -0700

    Adds user input to calculate factorial

--- Patterns/factorial_calculation.py
+++ Patterns/factorial_calculation.py
@@ -1,8 +1,9 @@
-# recursively finds the factorial of a number
+# recursively finds the factorial of a number using user input
 def factorial(number):
     if number == 0:
         return 1
     else:
         return factorial(number - 1) * number
-
-print(factorial(20))
+        
+x = int(input().strip())
+print(factorial(x))
