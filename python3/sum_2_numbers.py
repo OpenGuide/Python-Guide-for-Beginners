e4edb1ce333ed0bf8f04b0fdd95147cf72638e53
jatta-njie21 <laboyjatta@gmail.com>
2020-05-29 01:44:25 +0000

    Adding two numbers using python 3

---
+++ python3/sum_2_numbers.py
@@ -0,0 +1,15 @@
+# Adding two numbers in python3 
+
+
+#We ask the user for two numbers and store it as a float variable
+num1 = float(input("Input first number: "))
+num2 = float(input("Input second number: "))
+
+#No need the convert the numbers from string to float 
+
+#Now lets find the sum
+sum = num1 + num2
+
+#Concatination
+print("Num1 + Num2 =", sum)
+
