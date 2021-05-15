6e4563a84f59f763dbb68c40383ec9312db6feb0
Prathima Kadari <kadariprathima4@gmail.com>
2021-05-15 21:07:16 +0530

    Updated the quick sort program

--- SortingAlgorithms/quick_sort.py
+++ SortingAlgorithms/quick_sort.py
@@ -1,27 +1,31 @@
+# Quick Sort
 # Randomize Pivot to avoid worst case, currently pivot is last element
 def quickSort(A, si, ei):
     if si < ei:
-        pi=partition(A,si,ei)
-        quickSort(A,si,pi-1)
-        quickSort(A,pi+1,ei)
+        pi = partition(A, si, ei)
+        quickSort(A, si, pi-1)
+        quickSort(A, pi+1, ei)
 
 
+# Partition
 # Utility function for partitioning the array(used in quick sort)
 def partition(A, si, ei):
-    x = A[ei] # x is pivot, last element
+    x = A[ei]  # x is pivot, last element
     i = (si-1)
-    for j in range(si,ei):
+    for j in range(si, ei):
         if A[j] <= x:
             i += 1
             A[i], A[j] = A[j], A[i]
- 
-    A[i+1], A[ei] = A[ei], A[i+1]
-         
+    A[i+1], A[ei] = A[ei], A[i+1]      
     return i+1
-
-'''===Alternate Implementation==='''
+ 
+    
+#'Alternate Implementation'
 # Warning --> As this function returns the array itself, assign it to some
-# variable while calling. Example--> sorted_arr=quicksort(arr) 
+# variable while calling. Example--> sorted_arr=quicksort(arr)  
+# Quick sort
+
+
 def quicksort(arr):
     if len(arr) <= 1:
         return arr
